dd090c97741d17b63c2bc5909e3744cc5a16a0cf
fleger <florian6.leger@laposte.net>
2011-03-27 15:34:41 +0200

    Change indentation to 2 spaces

--- autoextension/__init__.py
+++ autoextension/__init__.py
@@ -5,51 +5,51 @@ Generic extension system.
 
 Overview
 ========
-    pyAutoExtension is a simple module designed to help you design modular applications by providing mechanisms to auto-detect no classes at the run-time.
+  pyAutoExtension is a simple module designed to help you design modular applications by providing mechanisms to auto-detect no classes at the run-time.
 
 The ExtensionClass concept
 ==========================
-    An extension class is simply a metaclass that keeps track of its instances. You can create new classes of extensions by subclassing ExtensionClass.
+  An extension class is simply a metaclass that keeps track of its instances. You can create new classes of extensions by subclassing ExtensionClass.
 
-    Usually, you should create an (abstact) base class to use as a basis for any new class (aka type or family) of extensions. Thus, each subclass of this base class will automatically have your custom ExtensionClass as metaclass.
+  Usually, you should create an (abstact) base class to use as a basis for any new class (aka type or family) of extensions. Thus, each subclass of this base class will automatically have your custom ExtensionClass as metaclass.
 
 Mini how-to
 ===========
 
-    >>> class MyExtensionClass(ExtensionClass):
-    ...     extensionPolicy = NameBasedBlackListExtensionPolicy("")      # Ignore all the classes whose name is an empty string
-    ...
-    >>> class MyAbstractExtension(object):
-    ...     __metaclass__ = MyExtensionClass
-    ...     name = ""
-    ...
-    >>> class MyExtension1(MyAbstractExtension):
-    ...     name = "MyExtension1"
-    ...
-    >>> class MyExtension2(MyAbstractExtension):
-    ...     name = "MyExtension2"
-    ...
-    >>> print(sorted([Extension.name for Extension in MyExtensionClass.getAvailableExtensions()]))
-    ['MyExtension1', 'MyExtension2']
+  >>> class MyExtensionClass(ExtensionClass):
+  ...   extensionPolicy = NameBasedBlackListExtensionPolicy("")      # Ignore all the classes whose name is an empty string
+  ...
+  >>> class MyAbstractExtension(object):
+  ...   __metaclass__ = MyExtensionClass
+  ...   name = ""
+  ...
+  >>> class MyExtension1(MyAbstractExtension):
+  ...   name = "MyExtension1"
+  ...
+  >>> class MyExtension2(MyAbstractExtension):
+  ...   name = "MyExtension2"
+  ...
+  >>> print(sorted([Extension.name for Extension in MyExtensionClass.getAvailableExtensions()]))
+  ['MyExtension1', 'MyExtension2']
 
 License
 =======
-    Generic extension system.
+  Generic extension system.
 
-    Copyright (C) 2009, 2011 Florian Léger
+  Copyright (C) 2009, 2011 Florian Léger
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see U{http://www.gnu.org/licenses/}.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see U{http://www.gnu.org/licenses/}.
 
 """
 
@@ -67,183 +67,183 @@ import operator
 # Exception classes
 
 class ExtensionError(Exception):
-    """
-    Base class for exceptions in this module.
-    """
-    pass
+  """
+  Base class for exceptions in this module.
+  """
+  pass
 
 
 class ExtensionNameError(ExtensionError):
-    """
-    Exception raised when there is no extension name.
-    """
-    pass
+  """
+  Exception raised when there is no extension name.
+  """
+  pass
 
 
 class ExtensionNameAlreadyExistsError(ExtensionError):
-    """
-    Exception raised when an extension with the same name already exists.
-    """
-    pass
+  """
+  Exception raised when an extension with the same name already exists.
+  """
+  pass
 
 
 # Policies
 
 class AbstractExtensionPolicy(object):
+  """
+  Abstract class for the extension policies.
+
+  An extension policy allows an ExtensionClass to avoid registering some of its instances based on some criteria.
+
+  You may define your own extension policies by subclassing this class and redefining the __call__ method.
+
+  You may compose several ExtensionPolicy instances together using the operators | (or), & (and), ^ (xor), and ~ (not).
+  """
+
+  def __call__(self, name, bases, namespace):
     """
-    Abstract class for the extension policies.
+    Test if new class will be registered in its ExtensionClass.
 
-    An extension policy allows an ExtensionClass to avoid registering some of its instances based on some criteria.
+    @param name:        Name of the new class.
+    @type name:         str
+    @param bases:       Base classes of the new class.
+    @type bases:        tuple
+    @param namespace:   The namespace of the new class.
+    @type namespace:    dict
 
-    You may define your own extension policies by subclassing this class and redefining the __call__ method.
+    @return: true if the class must be registered, false otherwise.
+    @rtype: boolean
 
-    You may compose several ExtensionPolicy instances together using the operators | (or), & (and), ^ (xor), and ~ (not).
+    @attention: name is the name given to the class during its definition, not the name of the extension. The name of the extension can be access though by using namespace["name"].
     """
+    pass
 
-    def __call__(self, name, bases, namespace):
-        """
-        Test if new class will be registered in its ExtensionClass.
+  def __and__(self, other):
+    if isinstance(other, AbstractExtensionPolicy):
+      return ComposedExtensionPolicy(self, other, operator.and_)
+    else:
+      return NotImplemented
 
-        @param name:        Name of the new class.
-        @type name:         str
-        @param bases:       Base classes of the new class.
-        @type bases:        tuple
-        @param namespace:   The namespace of the new class.
-        @type namespace:    dict
+  def __or__(self, other):
+    if isinstance(other, AbstractExtensionPolicy):
+      return ComposedExtensionPolicy(self, other, operator.or_)
+    else:
+      return NotImplemented
 
-        @return: true if the class must be registered, false otherwise.
-        @rtype: boolean
+  def __xor__(self, other):
+    if isinstance(other, AbstractExtensionPolicy):
+      return ComposedExtensionPolicy(self, other, operator.xor_)
+    else:
+      return NotImplemented
 
-        @attention: name is the name given to the class during its definition, not the name of the extension. The name of the extension can be access though by using namespace["name"].
-        """
-        pass
-
-    def __and__(self, other):
-        if isinstance(other, AbstractExtensionPolicy):
-            return ComposedExtensionPolicy(self, other, operator.and_)
-        else:
-            return NotImplemented
-
-    def __or__(self, other):
-        if isinstance(other, AbstractExtensionPolicy):
-            return ComposedExtensionPolicy(self, other, operator.or_)
-        else:
-            return NotImplemented
-
-    def __xor__(self, other):
-        if isinstance(other, AbstractExtensionPolicy):
-            return ComposedExtensionPolicy(self, other, operator.xor_)
-        else:
-            return NotImplemented
-
-    def __invert__(self):
-        return InvertedExtensionPolicy(self)
+  def __invert__(self):
+    return InvertedExtensionPolicy(self)
 
 
 class ComposedExtensionPolicy(AbstractExtensionPolicy):
+  """
+  Composition of two extension policies by a logical operator.
+  """
+  def __init__(self, operand1, operand2, binaryOperator):
     """
-    Composition of two extension policies by a logical operator.
+    Creates a new ComposedExtensionPolicy instance.
+
+    @param operand1:    First extension policy.
+    @type operand1:     AbstractExtensionPolicy
+    @param operand2:    Second extension policy.
+    @type operand2:     AbstractExtensionPolicy
+    @param binaryOperator:  Logical operator.
+    @type binaryOperator:   function
     """
-    def __init__(self, operand1, operand2, binaryOperator):
-        """
-        Creates a new ComposedExtensionPolicy instance.
+    self.operand1 = operand1
+    self.operand2 = operand2
+    self.binaryOperator = binaryOperator
 
-        @param operand1:    First extension policy.
-        @type operand1:     AbstractExtensionPolicy
-        @param operand2:    Second extension policy.
-        @type operand2:     AbstractExtensionPolicy
-        @param binaryOperator:  Logical operator.
-        @type binaryOperator:   function
-        """
-        self.operand1 = operand1
-        self.operand2 = operand2
-        self.binaryOperator = binaryOperator
-
-    def __call__(self, name, bases, namespace):
-        return self.binaryOperator(self.operand1(name, bases, namespace), self.operand2(name, bases, namespace))
+  def __call__(self, name, bases, namespace):
+    return self.binaryOperator(self.operand1(name, bases, namespace), self.operand2(name, bases, namespace))
 
 
 class InvertedExtensionPolicy(AbstractExtensionPolicy):
+  """
+  Invert the result of an extension policy.
+  """
+  def __init__(self, originalPolicy):
     """
-    Invert the result of an extension policy.
+    Creates a new InvertedExtensionPolicy instance.
+
+    @param originalPolicy:      The extension policy to invert.
+    @type originalPolicy:       AbstractExtensionPolicy
     """
-    def __init__(self, originalPolicy):
-        """
-        Creates a new InvertedExtensionPolicy instance.
+    self.originalPolicy = originalPolicy
 
-        @param originalPolicy:      The extension policy to invert.
-        @type originalPolicy:       AbstractExtensionPolicy
-        """
-        self.originalPolicy = originalPolicy
-
-    def __call__(self, name, bases, namespace):
-        return not self.originalPolicy(name, bases, namespace)
+  def __call__(self, name, bases, namespace):
+    return not self.originalPolicy(name, bases, namespace)
 
 
 class NameBasedWhiteListExtensionPolicy(AbstractExtensionPolicy):
+  """
+  Name-based white list extension policy.
+
+  Allows the registration of a new extension if it's name is in a white list.
+  """
+
+  def __init__(self, *args):
     """
-    Name-based white list extension policy.
+    Creates a new NameBasedWhiteListExtensionPolicy instance.
 
-    Allows the registration of a new extension if it's name is in a white list.
+    @param args:      White list of allowed extension names.
+    @type args:       str
     """
+    self.__whiteList = set(args)
 
-    def __init__(self, *args):
-        """
-        Creates a new NameBasedWhiteListExtensionPolicy instance.
+  @property
+  def whiteList(self):
+    """
+    The white list.
+    """
+    return self.__whiteList
 
-        @param args:      White list of allowed extension names.
-        @type args:       str
-        """
-        self.__whiteList = set(args)
-
-    @property
-    def whiteList(self):
-        """
-        The white list.
-        """
-        return self.__whiteList
-
-    def __call__(self, name, bases, namespace):
-        return namespace["name"] in self.whiteList
+  def __call__(self, name, bases, namespace):
+    return namespace["name"] in self.whiteList
 
 
 class NameBasedREExtensionPolicy(AbstractExtensionPolicy):
-    """
-    Name based extension policy using a regular expression.
+  """
+  Name based extension policy using a regular expression.
 
-    Allows the registration of a new extension if it's name match a given regular expression pattern.
-    """
-    def __init__(self, pattern):
-        self.pattern = pattern
+  Allows the registration of a new extension if it's name match a given regular expression pattern.
+  """
+  def __init__(self, pattern):
+    self.pattern = pattern
 
-    def __call__(self, name, bases, namespace):
-        return True if re.match(self.pattern, namespace["name"]) else False
+  def __call__(self, name, bases, namespace):
+    return True if re.match(self.pattern, namespace["name"]) else False
 
 
 class NameBasedBlackListExtensionPolicy(AbstractExtensionPolicy):
+  """
+  Name-based black list extension policy.
+
+  Allows the registration of a new extension if it's name is not in a black list.
+  """
+  def __init__(self, *args):
     """
-    Name-based black list extension policy.
+    Creates a new NameBasedBlackListExtensionPolicy instance.
 
-    Allows the registration of a new extension if it's name is not in a black list.
+    @param args:      Black list of forbidden extension names.
+    @type args:       str
     """
-    def __init__(self, *args):
-        """
-        Creates a new NameBasedBlackListExtensionPolicy instance.
+    self.__blackList = set(args)
 
-        @param args:      Black list of forbidden extension names.
-        @type args:       str
-        """
-        self.__blackList = set(args)
+  @property
+  def blackList(self):
+    """
+    The black list.
+    """
+    return self.__blackList
 
-    @property
-    def blackList(self):
-        """
-        The black list.
-        """
-        return self.__blackList
-
-    def __call__(self, name, bases, namespace):
-        return namespace["name"] not in self.blackList
+  def __call__(self, name, bases, namespace):
+    return namespace["name"] not in self.blackList
 
 
 DefaultExtensionPolicy = NameBasedBlackListExtensionPolicy
@@ -252,26 +252,26 @@ The default extension policy used when a new extension class is created.
 """
 
 class _MetaExtensionClass(type):
+  """
+  Metaclass of the extension classes.
+
+  For internal use.
+  """
+  def __new__(cls, name, bases, namespace):
+    if "extensionPolicy" not in namespace:
+      namespace["extensionPolicy"] = DefaultExtensionPolicy()
+    return type.__new__(cls, name, bases, namespace)
+
+  def __init__(self, name, bases, namespace):
+    type.__init__(self, name, bases, namespace)
+    self.__availableExtensions = {}
+
+  @property
+  def _availableExtensions(self):
     """
-    Metaclass of the extension classes.
-
-    For internal use.
+    Declared extensions.
     """
-    def __new__(cls, name, bases, namespace):
-        if "extensionPolicy" not in namespace:
-            namespace["extensionPolicy"] = DefaultExtensionPolicy()
-        return type.__new__(cls, name, bases, namespace)
-
-    def __init__(self, name, bases, namespace):
-        type.__init__(self, name, bases, namespace)
-        self.__availableExtensions = {}
-
-    @property
-    def _availableExtensions(self):
-        """
-        Declared extensions.
-        """
-        return self.__availableExtensions
+    return self.__availableExtensions
 
 
 class ExtensionClass(type):
@@ -286,31 +286,31 @@ class ExtensionClass(type):
     __metaclass__ = _MetaExtensionClass
 
     def __new__(cls, name, bases, namespace):
-        """
-        Creates a new extension class.
-        """
-        if "name" in namespace:
-            if cls.extensionPolicy(name, bases, namespace):
-                if namespace["name"] not in cls._availableExtensions:
-                    extension = type.__new__(cls, name, bases, namespace)
-                    cls._availableExtensions[namespace["name"]] = extension
-                    return extension
-                else:
-                    raise ExtensionNameAlreadyExistsError, "An extension named %s already exists at %s." %(namespace["name"], cls._availableExtensions[namespace["name"]])
-            else:
-                return type.__new__(cls, name, bases, namespace)
+      """
+      Creates a new extension class.
+      """
+      if "name" in namespace:
+        if cls.extensionPolicy(name, bases, namespace):
+          if namespace["name"] not in cls._availableExtensions:
+            extension = type.__new__(cls, name, bases, namespace)
+            cls._availableExtensions[namespace["name"]] = extension
+            return extension
+          else:
+            raise ExtensionNameAlreadyExistsError, "An extension named %s already exists at %s." %(namespace["name"], cls._availableExtensions[namespace["name"]])
         else:
-            raise ExtensionNameError, "%s has no name." %name
+          return type.__new__(cls, name, bases, namespace)
+      else:
+        raise ExtensionNameError, "%s has no name." %name
 
     @classmethod
     def getAvailableExtensions(cls):
-        """
-        Get the available extension classes.
+      """
+      Get the available extension classes.
 
-        @rtype:     tuple
-        @return:    A tuple containing all the declared extension classes.
-        """
-        return tuple(cls._availableExtensions.values())
+      @rtype:     tuple
+      @return:    A tuple containing all the declared extension classes.
+      """
+      return tuple(cls._availableExtensions.values())
 
 
 DEFAULT_EXTENSION_PATTERN = "[!_]*.py"
@@ -319,22 +319,22 @@ Default extension module file name fnmatch pattern.
 """
 
 def findExtensions(path, pattern=DEFAULT_EXTENSION_PATTERN):
-    """
-    Find all the extension modules in a given path.
+  """
+  Find all the extension modules in a given path.
 
-    @note:  File extensions are stripped from the result.
+  @note:  File extensions are stripped from the result.
 
-    @param path:    The search path.
-    @type path:     str
-    @param pattern: fnmatch search pattern.
-    @type pattern:  str
+  @param path:    The search path.
+  @type path:     str
+  @param pattern: fnmatch search pattern.
+  @type pattern:  str
 
-    @return:    An iterable containing all the module names.
-    @rtype:     iterable
-    """
-    return (os.path.basename(s).rsplit(".", 1)[0] for s in iglob(os.path.join(path, pattern)))
+  @return:    An iterable containing all the module names.
+  @rtype:     iterable
+  """
+  return (os.path.basename(s).rsplit(".", 1)[0] for s in iglob(os.path.join(path, pattern)))
 
 if __name__ == "__main__":
-    # Regression tests
-    import doctest
-    doctest.testmod()
+  # Regression tests
+  import doctest
+  doctest.testmod()
--- setup.py
+++ setup.py
@@ -10,18 +10,18 @@ Setup script.
 
 License
 =======
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see U{http://www.gnu.org/licenses/}.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see U{http://www.gnu.org/licenses/}.
 """
 
 __author__ = "Florian Léger"
@@ -33,24 +33,22 @@ __copyright__ = "2009, 2011 Florian Léger"
 from distutils.core import setup
 
 setup(name = "pyAutoExtension",
-    version = __version__,
-    description = "Generic extension system.",
-    author = __author__,
-    author_email = "florian6.leger@laposte.net",
-    url = "https://github.com/fleger/pyAutoExtension",
-    platforms = ['any'],
+  version = __version__,
+  description = "Generic extension system.",
+  author = __author__,
+  author_email = "florian6.leger@laposte.net",
+  url = "https://github.com/fleger/pyAutoExtension",
+  platforms = ['any'],
 
-    license = __license__,
-    use_2to3 = True,
+  license = __license__,
+  use_2to3 = True,
 
-    package_dir = {'autoextension': 'autoextension'},
-    packages = ['autoextension'],
+  package_dir = {'autoextension': 'autoextension'},
+  packages = ['autoextension'],
 
-    classifiers = [
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python'],
+  classifiers = [
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Development Status :: 5 - Production/Stable',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Programming Language :: Python'],
 )
-
-
